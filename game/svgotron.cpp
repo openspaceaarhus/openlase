c70cdc29b33256fd7cba9fabe71b7ae712369383
pedersen <jtp@cs.au.dk>
2015-08-14 08:17:12 +0200

    read svg add a sample file

--- game/svgotron.cpp
+++ game/svgotron.cpp
@@ -1,52 +1,41 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
-#include <random>
-#include <sstream>
+#include "laser.h"
 
 #include <libol.h>
 
+#include <iostream>
+#include <string>
+
+#define NANOSVG_ALL_COLOR_KEYWORDS  // Include full list of color keywords.
 #define NANOSVG_IMPLEMENTATION  // Expands implementation
 #include "nanosvg.h"
 
 using namespace std;
 
-
-void initol() {
-  // from examples/simple.c
-  OLRenderParams params;
-  
-  memset(&params, 0, sizeof params);
-  params.rate = 48000;
-  params.on_speed = 2.0/100.0;
-  params.off_speed = 2.0/20.0;
-  params.start_wait = 20;
-  params.start_dwell = 3;
-  params.curve_dwell = 0;
-  params.corner_dwell = 8;
-  params.curve_angle = cosf(30.0*(M_PI/180.0)); // 30 deg
-  params.end_dwell = 3;
-  params.end_wait = 7;
-  params.snap = 1/100000.0;
-  params.render_flags = RENDER_GRAYSCALE;
-  
-  if(olInit(3, 3000) < 0)
-    throw "hest";
-  
-  olSetRenderParams(&params);
-}
-
 int main(int argc, char *argv[]) {
-
-  initol();
-  auto w =  1.0f;
+  std::string filename{"hest.svg"};
   if (argc > 1) {
-    w = std::atof(argv[1]);
+    filename = argv[1];
   }
 
-  cout << "w = " << w;
-
+  struct NSVGimage* image;
+  image = nsvgParseFromFile(filename.c_str(), "px", 96);
+  printf("size: %f x %f\n", image->width, image->height);
+  // Use...
+  for (auto shape = image->shapes; shape != NULL; shape = shape->next) {
+    for (auto path = shape->paths; path != NULL; path = path->next) {
+      for (auto i = 0; i < path->npts-1; i += 3) {
+	float* p = &path->pts[i*2];
+	//	drawCubicBez(p[0],p[1], p[2],p[3], p[4],p[5], p[6],p[7]);
+	cout << p[0] << ", " << p[1] << ", " <<  p[2] << ", " << p[3] << ", " <<  p[4] << ", " << p[5] << ", " <<  p[6] << ", " <<  p[7] << ", " << endl;
+      }
+    }
+  }
+  // Delete
+  nsvgDelete(image);
+  
+  return 0;
+  
+  laser::initol();
   float time = 0;
   float ftime;
 
@@ -55,31 +44,12 @@ int main(int argc, char *argv[]) {
   while(1) {
     olLoadIdentity();
 
-    auto step = 2.0*w / 7.0f;
-    
-    for(int i=0; i < 7; i++) {
-      auto r = i & 1 ? 255 : 0;
-      auto g = i>>1 & 1 ? 255 : 0;
-      auto b = i>>2 & 1 ? 255 : 0;
-      auto c = (r << 16) | (g<<8) | b;
-
-      auto low = -w + i*step;
-      auto high = -w + (i+1)*step;
-      olRect(low,low, high,high,c);
-      if (i==0) {
-	olRect(low+.1,low+.1, high-.1,high-.1,C_RED);
-      }
-    }
-
-    olRect(0		, -w, 1.0 * step , -w + step, C_RED);
-    olRect(step		, -w, 2.0 * step , -w + step, C_GREEN);
-    olRect(2.0 * step	, -w, 3.0 * step , -w + step, C_BLUE);
 
     ftime = olRenderFrame(60);
     frames++;
     time += ftime;
 
-    printf("Frame time: %f, FPS:%f w=%f\n", ftime, frames/time, w);
+    printf("Frame time: %f, FPS:%f \n", ftime, frames/time);
 
   }
     
